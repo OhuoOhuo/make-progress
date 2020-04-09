e87c2ff8112c0b024d26b928cec7c9906ad60eb4
chengwenfa <18701660459@126.com>
2020-04-09 19:33:31 +0800

    提交

---
+++ src/arithmetic/principalLine/sortDemo.java
@@ -0,0 +1,228 @@
+package arithmetic.principalLine;
+
+import java.util.Arrays;
+
+public class sortDemo {
+
+    public static void main(String[] args){
+        int[] arr= {65,58,95,10,57,62,13,106,78,23,85};
+      // bobSort(arr);
+
+        //insertSort(arr);
+       // System.out.println("排序前："+ Arrays.toString(arr));
+
+        //quickSort(arr,0,arr.length-1);
+        margeSort(arr);
+        System.out.println("排序前："+ Arrays.toString(arr));
+    }
+
+    public static void margeSort(int[] arrs){
+
+        int[] temp = new int[arrs.length];
+
+        split(arrs,0,arrs.length-1,temp);
+
+    }
+
+    private static void split(int[] arrs, int left, int right, int[] temp) {
+        if(left >=right){
+            return;
+        }
+
+        int mid = left + (right -left)/2;
+
+        split(arrs,left,mid,temp);
+
+        split(arrs,mid+1,right,temp);
+
+        merge(arrs,left,mid,right,temp);
+
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int lstart = left;
+        int rstart = mid+1;
+        int tindex = 0;
+
+        while (lstart <=mid && rstart <=right){
+
+            if(arr[lstart] < arr[rstart]){
+                temp[tindex] =arr[lstart];
+                lstart++;
+            }else {
+                temp[tindex] = arr[rstart];
+                rstart++;
+            }
+            tindex++;
+        }
+
+        if(lstart<=mid){
+            while (lstart <=mid){
+                temp[tindex] = arr[lstart];
+                lstart++;
+                tindex++;
+            }
+        }else {
+            while (rstart<=right){
+                temp[tindex] = arr[rstart];
+                rstart++;
+                tindex++;
+            }
+        }
+        tindex = 0;
+        while (left <= right) {
+            arr[left] = temp[tindex];
+            left++;
+            tindex++;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void insertSort(int[] arrs){
+
+        for (int i = 1; i < arrs.length; i++) {
+            int temp =0;
+            int index=i;
+           while (arrs[index]<arrs[index-1]){
+                temp = arrs[index-1] ;
+                arrs[index-1] =arrs[index];
+                arrs[index] = temp;
+                index--;
+                if(index ==0){
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void choiceSort(int[] arrs){
+
+
+        for (int i = 0; i < arrs.length; i++) {
+
+            int min = arrs[i];
+            int index = i;
+
+            for (int j = i+1; j < arrs.length; j++) {
+                if(min> arrs[j]){
+                    min = arrs[j];
+                    index =j;
+                }
+
+            }
+
+            arrs[index] = arrs[i];
+            arrs[i] = min;
+
+        }
+
+    }
+
+
+    public static void bobSort(int[] arrs){
+
+        boolean flag =true;
+        for (int i = 0; i < arrs.length; i++) {
+            int k;
+            for (int j = i+1; j < arrs.length; j++) {
+                if(arrs[i] >arrs[j]){
+                    k= arrs[j];
+                    arrs[j] = arrs[i];
+                    arrs[i] = k;
+                    flag =false;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+    public static void quickSort(int[] arrs,int left,int right){
+        if(arrs ==null || arrs.length ==1){
+            return;
+        }
+        if(left <right){
+            int mid = partition2(arrs, left, right);
+            quickSort(arrs,left,mid-1);
+            quickSort(arrs,mid+1,right);
+        }
+    }
+
+
+    public static int partition(int[] arrs,int left,int right){
+        int mid =arrs[left];
+        while (left<right){
+            while (left <right &&arrs[right] > mid){
+                right --;
+            }
+            arrs[left] = arrs[right];
+            while (left< right &&arrs[left] <mid){
+                left ++;
+            }
+            arrs[right] = arrs[left];
+        }
+        arrs[left]=mid;
+        return left;
+
+    }
+
+    private static int partition2(int[] arrs,int left,int right){
+
+        int index =left;
+        int start = arrs[left] ;
+
+        for (int i = left+1; i <=right; i++) {
+            if(start >arrs[i]){
+                index++;
+                int p = arrs[i];
+                arrs[i] =arrs[index];
+                arrs[index] = p;
+
+            }
+
+        }
+        arrs[left]= arrs[index];
+
+        arrs[index] = start;
+
+
+        return index;
+    }
+
+    private static int partition3(int[] arr, int startIndex,
+                                 int endIndex) {
+         // 取第1个位置（ 也可以选择随机位置） 的元素作为基准元素
+         int pivot = arr[startIndex];
+         int mark = startIndex;
+
+         for(int i=startIndex+1; i<=endIndex; i++){
+             if(arr[i]<pivot){
+                 mark ++;
+                 int p = arr[mark];
+                 arr[mark] = arr[i];
+                 arr[i] = p;
+                 }
+             }
+
+         arr[startIndex] = arr[mark];
+         arr[mark] = pivot;
+         return mark;
+         }
+
+}
---
+++ src/arithmetic/principalLine/treeDemo.java
@@ -0,0 +1,114 @@
+package arithmetic.principalLine;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class treeDemo {
+    /**
+     * 根节点，子节点，叶子节点，父节点，兄弟节点，树的高度
+     * <p>
+     * 二叉树：
+     * 满二叉树，完成二叉树
+     * <p>
+     * 二叉树的遍历：
+     * 1.深度优先遍历（前序遍历，中序遍历，后序遍历）
+     * 2.广度优先遍历 (层序遍历)
+     */
+
+
+    public static void main(String[] args) {
+        LinkedList<Integer> inputList = new LinkedList(Arrays.asList(
+                new Integer[]{3, 2, 6,9, null, null, 10, null,null,4,null,null, 8, 11,null,null,12}));
+
+        TreeNode treeNode = createBinaryTree(inputList);
+
+        System.out.println("前序遍历：");
+        preOrder(treeNode);
+        System.out.println("中序遍历：");
+        midOrder(treeNode);
+        System.out.println("后序遍历：");
+        postOrder(treeNode);
+        System.out.println("广度优先：");
+        levelOrder(treeNode);
+
+
+    }
+
+    private static void levelOrder(TreeNode root) {
+        if(root==null){
+            System.out.println("树为空");
+        }
+        Queue<TreeNode> queuetree = new LinkedList<>();
+        queuetree.offer(root);
+        while (!queuetree.isEmpty()){
+            TreeNode node = queuetree.poll();
+            System.out.print(node.data+" ");
+            if(node.leftChild !=null){
+                queuetree.offer(node.leftChild);
+            }
+            if(node.rightChild !=null){
+                queuetree.offer(node.rightChild);
+            }
+        }
+
+    }
+
+
+    private static void postOrder(TreeNode treeNode) {
+        if(treeNode ==null){
+            return;
+        }
+
+        postOrder(treeNode.leftChild);
+        postOrder(treeNode.rightChild);
+        System.out.print(treeNode.data+" ");
+    }
+
+    private static void midOrder(TreeNode treeNode) {
+        if (treeNode ==null){
+            return;
+        }
+        midOrder(treeNode.leftChild);
+        System.out.print(treeNode.data+" ");
+        midOrder(treeNode.rightChild);
+    }
+
+    private static void preOrder(TreeNode treeNode) {
+        if (treeNode ==null){
+            return;
+        }
+        System.out.print(treeNode.data+" ");
+        preOrder(treeNode.leftChild);
+        preOrder(treeNode.rightChild);
+    }
+
+
+    private static TreeNode createBinaryTree(LinkedList<Integer>
+                                                    inputList) {
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        Integer data = inputList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.leftChild = createBinaryTree(inputList);
+            node.rightChild = createBinaryTree(inputList);
+        }
+        return node;
+    }
+
+
+    private static class TreeNode {
+        int data;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+
+}
