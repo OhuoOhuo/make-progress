9dc5dd71337742dcf52f40946a219c73b85965b9
chengwenfa <chengwenfa@allinmd.cn>
2020-10-14 14:33:11 +0800

    java8

---
+++ src/arithmetic/zuo/class01/BSAwesome.java
@@ -0,0 +1,21 @@
+package arithmetic.zuo.class01;
+
+public class BSAwesome {
+
+
+    public static int getLessIndex(int[] arr) {
+        if(arr==null || arr.length<=1){
+            return -1;
+        }
+        if(arr[0] < arr[1]){
+            //return arr
+        }
+
+
+        return -1;
+    }
+
+
+
+
+}
---
+++ src/arithmetic/zuo/class01/BSExist.java
@@ -0,0 +1,80 @@
+package arithmetic.zuo.class01;
+
+import java.util.Arrays;
+
+
+/**
+ *
+ */
+public class BSExist {
+
+
+    public static boolean exist(int[] sortedArr, int num) {
+        if(sortedArr ==null || sortedArr.length ==0){
+            return false;
+        }
+        int left =0;
+        int right = sortedArr.length -1;
+        int mid = 0;
+        while (left <= right){
+            mid = (left +right)/2;
+
+            if(sortedArr[mid] ==num){
+                return true;
+            }else if(sortedArr[mid] >num){
+                right = mid -1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return false;
+    }
+
+    // for test
+    public static boolean test(int[] sortedArr, int num) {
+        for(int cur : sortedArr) {
+            if(cur == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+   public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != exist(arr, value)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr) +"::" +value);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+/*    public static void main(String[] args) {
+        int[] arr={-4,0,31};
+        exist(arr,-4);
+
+    }*/
+
+
+
+
+}
